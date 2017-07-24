3276c58c3ecbe49bf70f29a3268e4740dd98bb1e
Алексей Знаменский <azum23@gmail.com>
2017-07-24 11:42:43 +0300

    002

--- T11_operatorOverride_01/Cyborg.cs
+++ T11_operatorOverride_01/Cyborg.cs
@@ -8,7 +8,56 @@ namespace T11_operatorOverride_01
 {
     class Cyborg
     {
+        public string cyborgName;
         List<Element> bodyParts = new List<Element>();
+
+        public Cyborg(string _name)
+        {
+            cyborgName = _name;
+        }
+
+        public Cyborg(string _name, List<Element> parts) : this (_name)
+        {
+            bodyParts = parts;
+        }
+
+        // Добавление новых элементов к механизму
+        public void AddElement(Element e)
+        {
+            bodyParts.Add(e);
+        }
+
+        // Выводим описание киборга с именем и каждой деталью
+        public override string ToString()
+        {
+            StringBuilder s = new StringBuilder();
+            s.AppendLine($"Киборг {cyborgName} с убийственным функциналом:");
+            int id = 1;
+            foreach (Element e in bodyParts)
+            {
+                s.AppendLine($"[{id}] механизм {e.name} с функцией {e.function}");
+                id++;
+            }
+            return s.ToString();
+        }
+
+
+        // Складываем киборгов суммируя добавляем в нового каждую деталь от предыдущего
+        public static Cyborg operator +(Cyborg c1, Cyborg c2)
+        {
+            List<Element> newParts = new List<Element>();
+            newParts.AddRange(c1.bodyParts);
+            newParts.AddRange(c2.bodyParts);
+
+            Cyborg newCyborg = new Cyborg(c1.cyborgName + c2.cyborgName, newParts);
+            return newCyborg;
+        }
+
+        public static Cyborg operator ++(Cyborg c1)
+        {
+            c1.bodyParts.Add(new Element("Пустой слот","ничего не далет, ждет"));
+            return c1;
+        }
 
     }
 }
--- T11_operatorOverride_01/Program.cs
+++ T11_operatorOverride_01/Program.cs
@@ -12,6 +12,32 @@ namespace T11_operatorOverride_01
     {
         static void Main(string[] args)
         {
+            Cyborg c1 = new Cyborg("Crusher");
+            c1.AddElement(new Element("Миксер", "Перемалывает в труху"));
+            c1.AddElement(new Element("Пулемет", "Стреляет патронами"));
+
+            Cyborg c2 = new Cyborg("RepairMan", new List<Element>()
+            {
+                new Element("Длань Божья", "Лечит все к чему прикоснеться"),
+                new Element("Бутыль святой воды", "Заливает ржавчину"),
+            });
+
+            var c3 = c1 + c2;
+            Console.WriteLine(c3.ToString());
+            Console.WriteLine(c3.GetHashCode());
+            
+            
+            // проверяем ++
+            c3++;
+            Console.WriteLine(c3.ToString());
+            Console.WriteLine(c3.GetHashCode()); // проверяем тот же обьект?
+
+            // проверяем +=
+            c3 += c1;
+            Console.WriteLine(c3.ToString());
+            Console.WriteLine(c3.GetHashCode());
+
+            Console.ReadLine();
         }
     }
 }
