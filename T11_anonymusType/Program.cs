113ad56ae4072782cb5a862c5d11724b07fc3d0c
Алексей Знаменский <azum23@gmail.com>
2017-07-24 14:28:02 +0300

    final

---
+++ T11_anonymusType/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T11_anonymusType
+{
+    class Program
+    {
+        static void Reflector(object obj)
+        {
+            Console.WriteLine(obj.GetType().Name);
+            Console.WriteLine(obj.GetType().BaseType);
+        }
+
+        static void Main(string[] args)
+        {
+            var car = new { Name = "Lambo", maxSpeed = 300 };
+            Console.WriteLine(car.Name);
+            Console.WriteLine(car.ToString());
+            Reflector(car);
+            var car2 = new { Name = "BMW", maxSpeed = 200 };
+            Console.WriteLine(car.Equals(car2));
+            Console.ReadLine();
+        }
+    }
+}
